f9e4e15db529f940c22f31b50a7368dcac2772b6
raunit-dev <raunitjaiswal510@gmail.com>
2025-02-01 14:57:51 +0530

    actual assign

--- practiseTS/assign1.ts
+++ practiseTS/assign1.ts
@@ -1,24 +1,36 @@
-function swap<T>(z: T[], v: T[]): T[][] {
-    let temp: T = z[0];
-    z[0] = v[0];
-    v[0] = temp;
-    return [z, v];
+// function swap<T>(z: T[], v: T[]): T[][] {
+//     let temp: T = z[0];
+//     z[0] = v[0];
+//     v[0] = temp;
+//     return [z, v];
+// }
+
+// let a: number[] = [1, 2, 3, 4, 5];
+// let b: number[] = [10, 11, 12, 13, 14];
+// let c: string[] = ["raunit", "rishi"];
+// let d: string[] = ["ajay", "sona"];
+// let e: boolean[] = [true, true];
+// let f: boolean[] = [false, false];
+
+// let [ansA, ansB] = swap(a, b);
+// let [ansC, ansD] = swap(c, d); 
+// let [ansE, ansF] = swap(e, f);
+
+// console.log(ansA);
+// console.log(ansB);
+// console.log(ansC);
+// console.log(ansD);
+// console.log(ansE);
+// console.log(ansF);
+
+function swappie<T>(a:T,b:T) {
+  return [b,a];
 }
 
-let a: number[] = [1, 2, 3, 4, 5];
-let b: number[] = [10, 11, 12, 13, 14];
-let c: string[] = ["raunit", "rishi"];
-let d: string[] = ["ajay", "sona"];
-let e: boolean[] = [true, true];
-let f: boolean[] = [false, false];
+let answer=swappie(1,2);
+let answer2=swappie("raunit","rishi");
 
-let [ansA, ansB] = swap(a, b);
-let [ansC, ansD] = swap(c, d); 
-let [ansE, ansF] = swap(e, f);
+console.log(answer);
+console.log(answer2);
+ 
 
-console.log(ansA);
-console.log(ansB);
-console.log(ansC);
-console.log(ansD);
-console.log(ansE);
-console.log(ansF);
